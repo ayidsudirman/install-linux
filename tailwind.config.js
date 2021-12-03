cca56b03a2c340c0d0a36ae6b63c9025f13bea8d
ayidsudirman <ayidsudirman@protonmail.com>
2021-12-03 17:43:12 +0800

    creating section

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#DC2626",
+        secondary: {
+          100: "#D1FAE5",
+          200: "#A7F3D0",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

cd6198aa3029c37d45e659f0a60b5985c1e47425
ayidsudirman <ayidsudirman@protonmail.com>
2021-12-06 05:45:18 +0800

    purging styles with JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  purge: [],
+  mode: "jit",
+  purge: ["./*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        primary: "#DC2626",
-        secondary: {
-          100: "#D1FAE5",
-          200: "#A7F3D0",
-        },
-      },
       fontFamily: {
         body: ["Nunito"],
       },
